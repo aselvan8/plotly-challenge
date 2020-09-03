8bb55d5d68b4cead76d8239d45e90977d7ba90f4
aselvan8 <65464009+aselvan8@users.noreply.github.com>
2020-09-03 00:34:20 -0400

    add bar and bubble plot

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,6 +13,8 @@ function init() {
     });
 };
 
+init();
+
 // function to get demographic info
 function getinfo(id) {
 
@@ -42,7 +44,65 @@ function getplot(id) {
         // filter sample value by id
         var samples = data.samples.filter(d => d.id == id)[0];
 
+        // get top 10 sample values
+        var samplevalues = samples.sample_values.slice(0, 10).reverse();
 
+        // get top 10 otu_ids
+        var ids = samples.otu_ids.slice(0, 10).reverse();
+        var otuids = ids.map(d => "OTU " + d);
+
+        var otulables = samples.otu_labels.slice(0, 10);
+
+        // trace for the bar chart
+        var trace = {
+            x: samplevalues,
+            y: otuids,
+            text: otulables,
+            type: "bar",
+            orientation: "h"
+        };
+
+        var data = [trace];
+
+        // layout to set plots layout
+        var layout = {
+            title: "Top 10 OTU",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 30,
+                b: 20
+            }
+        };
+
+        // present the bar plot
+        Plotly.newPlot("bar", data, layout)
+
+
+        // trace for the bubble chart
+        var trace1 = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+
+        };
+
+        // layout for the bubble plot
+        var layout1 = {
+            xaxis:{title: "OTU"},
+            height: 700,
+            width: 1000
+        };
+
+        var data1 = [trace1];
+
+        // present the bar plot
+        Plotly.newPlot("bubble", data1, layout1)
 
     });
 };
@@ -52,5 +112,3 @@ function optionChanged(id) {
     getplot(id);
     getinfo(id);
 };
-
-init();
